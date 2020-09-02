9ce9287ae2e456ed539fbd652a3537ba713836d4
GowoonJ <jgw971229@naver.com>
2020-09-02 19:25:12 +0900

    lock.wait - timeout 2000ms success

--- src/main/java/com/moberan/exam2020/Main.java
+++ src/main/java/com/moberan/exam2020/Main.java
@@ -7,27 +7,32 @@ public class Main {
 		String result = tasks();
 		System.out.println(result);
 	}
+
 	private static final Object lock = new Object();
+
 	private static String tasks(){
 		TestLibrary lib = new TestLibrary();
 		final String[] result = new String[1];
-		synchronized (lib){
+
+		final boolean[] lockOff = {false};
+
+		synchronized (lock){
 			lib.firstTask(new Task() {
 				@Override
 				public  void taskCallback(String s) {
 					result[0] = lib.secondTask(s);
+					lockOff[0] = true;
 					System.out.println("second Task: "+result[0]);
 					System.out.println(s);
-					notifyAll();
 				}
 			});
 			try {
-				lib.wait();
+				lock.wait(2000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			if (lockOff[0])	lock.notify();
 		}
-		System.out.println("second Task2 : "+result[0]);
 		return result[0];
 	}
 }
